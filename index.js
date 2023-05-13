eeab0f174aaacf60888047efe552d9ffb12f168d
Ragonesi Alessio <aleragonesi04@gmail.com>
2023-05-13 16:42:32 +0200

    Svolgimento esercizio Promises Lucky Draw

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const fs = require("fs");
+const players = ["Joe", "Caroline", "Sabrina"]
 
-const data = new Uint8Array(Buffer.from("Prova fs"))
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+        const win = Boolean(Math.round(Math.random()));
 
-fs.writeFile("./TestFs.txt", data, (error) => {
-    if(error) console.log(error);
-})
+        process.nextTick(() => {
+            if (win) {
+                resolve(`${player} won a prize in the draw!`);
+            } else {
+                reject(new Error(`${player} lost the draw.`));
+            }
+        });
+    });
+}
+
+players.forEach((player) => luckyDraw(player).then(console.log).catch(console.log));
+
+
